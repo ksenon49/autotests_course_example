0a635fc297efa1a80c6cfba0f6e40b01ecdd9dd1
ka.chernyh <ka.chernyh@tensor.ru>
2023-05-31 21:33:05 +0300

    седьмое обновленное

--- task2.py
+++ task2.py
@@ -1,61 +1,81 @@
-# Напишите класс Segment
-# Для его инициализации нужно два кортежа с координатами точек (x1, y1), (x2, y2)
+# Напишите класс PersonInfo
+# Экземпляр класса создается из следующих атрибутов:
+# 1. Строка - "Имя Фамилия"
+# 2. Число - возраст сотрудника
+# 3. Подразделения от головного до того, где работает сотрудник.
 # Реализуйте методы класса:
-# 1. length, который возвращает длину нашего отрезка, с округлением до 2 знаков после запятой
-# 2. x_axis_intersection, который возвращает True, если отрезок пересекает ось абцисс, иначе False
-# 3. y_axis_intersection, который возвращает True, если отрезок пересекает ось ординат, иначе False
+# 1. short_name, который возвращает строку Фамилия И.
+# 2. path_deps, возвращает путь "Головное подразделение --> ... --> Конечное подразделение"
+# 3. new_salary, Директор решил проиндексировать зарплаты, и новая зарпалата теперь вычисляет по формуле:
+# 1337*Возраст*суммарное кол-во вхождений трех наиболее часто встречающихся букв из списка подразделений
+# (регистр имеет значение "А" и "а" - разные буквы)
 # Например (Ввод --> Вывод) :
-# Segment((2, 3), (4, 5)).length() --> 2.83
-# Segment((-2, -3), (4, 5)).x_axis_intersection() --> True
-# Segment((-2, -3), (-4, -5)).y_axis_intersection() --> False
-import math
+# PersonInfo('Александр Шленский',
+#            32,
+#            'Разработка', 'УК', 'Автотесты').short_name() --> 'Шленский А.'
+# PersonInfo('Александр Шленский',
+#            32,
+#            'Разработка', 'УК', 'Автотесты').path_deps() -->
+#            'Разработка --> УК --> Автотесты'
+# PersonInfo('Александр Шленский', 32, 'Разработка', 'УК', 'Автотесты').new_salary() --> 385056 т.к.
+# т.к. буква "т" встречается 4 раза, "а" 3 раза, 'о' 2 раза, остальные по одной. Сумма трёх самых частых букв 4+3+2 = 9.
+# 1337*32*9 = 385056
 
+class PersonInfo:
 
-class Segment():
+    def __init__(self, name, age, department):
+        self.name = name
+        self.age = age
+        self.department = department
 
-    def __init__(self, seg1, seg2):
-        self.seg1 = seg1
-        self.seg2 = seg2
+    def short_name(self):
+        name = self.name.split()
+        return name[1] + ' ' + name[0][0]
 
-    def length(self):
-        x1, y1 = self.seg1
-        x2, y2 = self.seg2
-        return round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5, 2)
+    def path_deps(self):
+        return '--> '.join(self.department)
 
-    def x_axis_intersection(self):
-        x1, y1 = self.seg1
-        x2, y2 = self.seg2
-        if y1 * y2 < 0:
-            return True
-        else:
-            return False
-
-    def y_axis_intersection(self):
-        x1, y1 = self.seg1
-        x2, y2 = self.seg2
-        if x1 * x2 < 0:
-            return True
-        else:
-            return False
-
-
+    def new_salary(self):
+        from collections import Counter
+        age = self.age
+        department = self.department
+        d = {}
+        d.update(Counter(department).most_common(3))
+        sum_letters = sum(d.values())
+        res = 1337 * age * sum_letters
+        return res
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 
 
-data = [Segment((2, 3), (4, 5)).length,
-        Segment((1, 1), (1, 8)).length,
-        Segment((0, 0), (0, 1)).length,
-        Segment((15, 1), (18, 8)).length,
-        Segment((-2, -3), (4, 5)).x_axis_intersection,
-        Segment((-2, -3), (-4, -2)).x_axis_intersection,
-        Segment((0, -3), (4, 5)).x_axis_intersection,
-        Segment((2, 3), (4, 5)).y_axis_intersection,
-        Segment((-2, -3), (4, 5)).y_axis_intersection,
-        Segment((-2, 3), (4, 0)).y_axis_intersection
+first_person = PersonInfo('Александр Шленский', 32, 'Разработка', 'УК', 'Автотесты')
+fourth_person = PersonInfo('Иван Иванов', 26, 'Разработка')
+second_person = PersonInfo('Пётр Валерьев', 47, 'Разработка', 'УК')
+third_person = PersonInfo('Макар Артуров', 51, 'Разработка', 'УК', 'Нефункциональное тестирование', 'Автотестирование')
+
+data = [first_person.short_name,
+        second_person.short_name,
+        third_person.short_name,
+        fourth_person.short_name,
+
+        first_person.path_deps,
+        second_person.path_deps,
+        third_person.path_deps,
+        fourth_person.path_deps,
+
+        first_person.new_salary,
+        second_person.new_salary,
+        third_person.new_salary,
+        fourth_person.new_salary
         ]
 
 
-test_data = [2.83, 7.0, 1.0, 7.62, True, False, True, False, True, True]
+test_data = ['Шленский А.', 'Валерьев П.', 'Артуров М.', 'Иванов И.',
+
+             'Разработка --> УК --> Автотесты',
+             'Разработка --> УК',
+             'Разработка --> УК --> Нефункциональное тестирование --> Автотестирование',
+             'Разработка',
+             385056, 314195, 1227366, 173810]
 
 for i, d in enumerate(data):
     assert_error = f'Не прошла проверка для метода {d.__qualname__} экземпляра с атрибутами {d.__self__.__dict__}'
