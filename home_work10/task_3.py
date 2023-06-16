1ab265c2a6ea36447adb5b33a624491f0aeef8ea
ka.chernyh <ka.chernyh@tensor.ru>
2023-06-16 12:24:27 +0300

    десятое со вторым, еще раз

--- home_work10/task_3.py
+++ home_work10/task_3.py
@@ -2,7 +2,6 @@
 # Промаркируйте 1 параметр из выборки как smokе, а 1 набор данных скипните
 
 import pytest
-import test_task102
 
 
 def all_division(*arg1):
@@ -17,7 +16,8 @@ def all_division(*arg1):
     pytest.param(8, 2, 4, marks=pytest.mark.smoke('smoke')),
     pytest.param(2.3, 1, 2.3, marks=pytest.mark.skip('not work')),
     (3, 0.5, 6),
-    (8, 0, ZeroDivisionError)],
-    ids=['smoke', 'not work', 'positive_with_float', 'zero division'])
+    (8, 0, ZeroDivisionError),
+    (-20, 5, -4)],
+    ids=['smoke', 'not work', 'positive_with_float', 'zero division', 'with_minus'])
 def test_smoke(a, b, result):
     assert all_division(a, b) == result
